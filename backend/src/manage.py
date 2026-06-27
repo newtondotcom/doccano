463229d58ea6dee602604af1a5c634fa22ddfff8
newtondotcom <asphalt9fr@gmail.com>
2026-06-27 23:05:02 +0200

    feat: new backend structure with uv workspaces support
    `auto-labeling-pipeline` package is included in the repo now

--- backend/src/manage.py
+++ backend/src/manage.py
@@ -2,6 +2,9 @@
 import os
 import sys
 
+
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'src'))
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
     try:
